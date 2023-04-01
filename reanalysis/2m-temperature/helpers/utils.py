dff243df0a3a6c2813f74c2591772661bbfd7a12
adisaljusi <adis.aljusi@hotmail.com>
2023-04-01 17:23:29 +0200

    zonal statistics of aggregated raster values to vector geometries

---
+++ reanalysis/2m-temperature/helpers/utils.py
@@ -0,0 +1,12 @@
+def kelvin_to_celcius(kelvin: float) -> float:
+    """
+    Converts Kelvin t
+
+    :param kelvin: Kelvin value to be converted
+    :type kelvin: float
+
+    :returns: Float value converted to celcius
+    :rtype: float
+    """
+
+    return kelvin - 273.15
